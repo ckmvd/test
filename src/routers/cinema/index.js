a11c576e8872117748cf01cb1726ee94c10d856a
ckmvd <614382634@qq.com>
2019-07-14 14:39:23 +0800

    add createcomponents

--- src/routers/cinema/index.js
+++ src/routers/cinema/index.js
@@ -1,4 +1,10 @@
 export default {
-    path:"/cinema",
-    component:()=>import('@/views/Cinema')
+    path:'/cinema',
+    component:()=>import('@/views/Cinema'),
+    // children:[
+    //     {
+    //         path:'cilist',
+    //         component:()=>import('@/components/CiList')
+    //     }
+    // ]
 }
